f5d656eb6e64ce376d1ef7d98bcd43bb354f9d68
orkeilius <orkeilius@users.noreply.github.com>
2023-02-11 15:13:28 +0100

    add link in nav

--- module/header.php
+++ module/header.php
@@ -3,13 +3,12 @@
     <h1>Vp enterprise</h1>
     <nav>
         <a class="navItem" href="/">Accueil</a>
-        <a class="navItem" href="/">Match</a>
-        <a class="navItem" href="/">Joueur</a>
-        <a class="navItem" href="/">Stade</a>
-        <a class="navItem" href="/">Commentateur</a>
+        <a class="navItem" href="match.php">Match</a>
+        <a class="navItem" href="joueur.php">Joueur</a>
+        <a class="navItem" href="stade.php">Stade</a>
+        <a class="navItem" href="commentateur.php">Commentateur</a>
         <div style="flex-grow:1"></div>
-        <?php if (getUserAccess() == 0) {
-            var_dump($_SESSION); ?>
+        <?php if (getUserAccess() == 0) { ?>
 
             <a class="navItem" href="edit.php">edit</a>
             <a class="navItem" href="api/logout.php">logout</a>
