95653746192f81bb31eccb05c651c404d46a1791
orkeilius <orkeilius@users.noreply.github.com>
2024-04-17 15:50:18 +0000

    add head template

---
+++ template/head.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="/style/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" type="image/ico" href="favicon.png">
---
+++ template/header.php
@@ -0,0 +1,19 @@
+<?php ?>
+<header>
+    <h1 style="padding-left: 10px"> Le tournois octogone</h1>
+    <nav>
+        <a class="navItem" href="/">Accueil</a>
+        <a class="navItem" href="match.php">Match</a>
+        <a class="navItem" href="joueur.php">Joueur</a>
+        <a class="navItem" href="stade.php">Stade</a>
+        <a class="navItem" href="commentateur.php">Commentateur</a>
+        <div style="flex-grow:1"></div>
+        <?php if (DbConnection::isUserAdmin()) { ?>
+
+            <a class="navItem" href="/admin">admin panel</a>
+            <a class="navItem" href="/logout">logout</a>
+        <?php } else { ?>
+            <a class="navItem" href="/login">login</a>
+        <?php } ?>
+    </nav>
+</header>
